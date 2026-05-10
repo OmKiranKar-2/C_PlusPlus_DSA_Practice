eaae5943adfc3acf540894ccab374c0b0a833a6a
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-05-10 23:56:15 +0530

    Linear Search

--- Array/Array4.cpp
+++ Array/Array4.cpp
@@ -1,7 +1,22 @@
 //Linear Search
 #include<iostream>
+
+int linearSearch(short int arr[], short int target)
+{
+    for(int i=0;i<7;i++)
+    {
+        if(arr[i]==target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-
+    short int arr[7]={4,2,7,8,1,2,5};
+    short int target = 8;
+    std::cout<<"Index is "<<linearSearch(arr,target);
     return 0;
 }
