1d8a0924da75056f5e8df30e0f0ff1cf3297eb47
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-05-13 23:15:24 +0530

    Brute Force

--- Leetcode/53-Maximum Subarray.cpp
+++ Leetcode/53-Maximum Subarray.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
+#include<vector>
+#include<climits>
 int main()
 {
-
+    // Brute Force
+    std::vector<int> vec = {-2,1,-3,4,-1,2,1,-5,4}; //Maximum Subarray -> 4,-1,2,1 -> 6
+    int max = INT_MIN;
+    for(int i=0;i<vec.size();i++)
+    {
+        int temp = 0;
+        for(int j=i;j<vec.size();j++)
+        {
+            temp += vec[j];
+            max = std::max(temp,max);
+            
+        }
+    }
+    std::cout<<"Maximum subarray: "<<max;
     return 0;
 }
