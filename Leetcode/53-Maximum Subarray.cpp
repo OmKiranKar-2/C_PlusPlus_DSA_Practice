343ad5b5d6a80a987edf153a30c6a751ef3a480e
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-05-13 23:52:56 +0530

    Kadane Algorithm

--- Leetcode/53-Maximum Subarray.cpp
+++ Leetcode/53-Maximum Subarray.cpp
@@ -3,19 +3,30 @@
 #include<climits>
 int main()
 {
-    // Brute Force
+    // Kadane's Algorithm
     std::vector<int> vec = {-2,1,-3,4,-1,2,1,-5,4}; //Maximum Subarray -> 4,-1,2,1 -> 6
-    int max = INT_MIN;
+    int max=vec[0],add=0;
     for(int i=0;i<vec.size();i++)
     {
-        int temp = 0;
-        for(int j=i;j<vec.size();j++)
-        {
-            temp += vec[j];
-            max = std::max(temp,max);
-            
-        }
+        add += vec[i];
+        if(add < vec[i]) add=vec[i];
+        max = std::max(max,add);
     }
-    std::cout<<"Maximum subarray: "<<max;
+    std::cout<<"Maximum Subarray: "<<max;
+
+    // // Brute Force
+    // std::vector<int> vec = {-2,1,-3,4,-1,2,1,-5,4};
+    // int max = INT_MIN;
+    // for(int i=0;i<vec.size();i++)
+    // {
+    //     int temp = 0;
+    //     for(int j=i;j<vec.size();j++)
+    //     {
+    //         temp += vec[j];
+    //         max = std::max(temp,max);
+            
+    //     }
+    // }
+    // std::cout<<"Maximum subarray: "<<max;
     return 0;
 }
