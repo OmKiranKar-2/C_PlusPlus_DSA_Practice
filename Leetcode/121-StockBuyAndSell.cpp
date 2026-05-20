111c902c22dd8cb11e27601812cf7d45d0d00524
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-05-21 02:20:04 +0530

    Stock Buy and Sell

--- Leetcode/121-StockBuyAndSell.cpp
+++ Leetcode/121-StockBuyAndSell.cpp
@@ -3,10 +3,18 @@
 #include<vector>
 int main()
 {
-    std::vector<int> prices = {7,1,5,3,6,4}; // Max Profit: 5, buy on 2nd day, sell on 5th day
+    std::vector<int> prices = {7,2,1,5,3,6,4}; // Max Profit: 5, buy on 2nd day, sell on 5th day
     int profit = 0;
 
-    // // Brute Force Approach
+    // Optimized Approach -> TC: O(n)
+    int bestBuy=prices[0];
+    for(int i=1;i<prices.size();i++)
+    {
+        profit = std::max(profit,prices[i]-bestBuy);
+        bestBuy = std::min(bestBuy,prices[i]);
+    }
+
+    // // Brute Force Approach -> TC: O(n^2)
     // for(int i=0;i<prices.size();i++)
     // {
     //     for(int j=i+1;j<prices.size();j++)
