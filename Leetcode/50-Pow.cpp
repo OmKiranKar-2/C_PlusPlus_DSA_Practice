a20794add62097a746611c7eab0e51c8e3e2248f
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-05-20 23:37:09 +0530

    Binary Exponentation-Updated

--- Leetcode/50-Pow.cpp
+++ Leetcode/50-Pow.cpp
@@ -9,6 +9,43 @@ int main()
     std::cin>>num;
     std::cout<<"Enter power: ";
     std::cin>>pow;
+
+    // Optimized Approach
+
+    // // Better Approach than previous one, though TC can be more optimized
+    // int dupPow = pow,count=0;
+    // double powSum = num, twoSum=1;
+    // while(dupPow > 1 || dupPow<-1)
+    // {
+    //     count++;
+    //     dupPow /= 2;
+    // }
+    // for(int i=0;i<count;i++)
+    // {
+    //     powSum *= powSum;
+    //     twoSum *= 2;
+    // }
+    // if(dupPow == 1)
+    // {
+    //     count = pow - twoSum;
+    // }
+    // else
+    // {
+    //     count = pow + twoSum;
+    //     count = -count;  
+    // }
+    // for(int i=0;i<count;i++)
+    // {
+    //     powSum *= num;
+    // }
+    // if(dupPow == -1)
+    // {
+    //     powSum = 1/powSum;
+    // }
+    // if(pow==0) powSum=1;
+    // std::cout<<powSum;
+
+    
     // // Longer approach -> Worse TC [Time Complexity]
     // float dupNum = num;
     // if(pow>1)
