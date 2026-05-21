6f33d25b8759dc8a3e12530dcf784e206c4a3461
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-05-21 14:00:08 +0530

    Optimized Approach - 2 pointer

--- Leetcode/11-ContainerWithMostWater.cpp
+++ Leetcode/11-ContainerWithMostWater.cpp
@@ -5,15 +5,32 @@ int main()
 {
     std::vector<int> heights = {1,8,6,2,5,4,8,3,7}; // Container between Index 1 and 8 gives maximum water
     int maxWater=0, minIndex;
-    //Brute Force Approach
-    for(int i=0;i<heights.size();i++)
+    // Optmized Approach -> TC: O(n) [Use Two Pointer Approach]
+    int leftPtr=0, rightPtr=heights.size()-1;
+    while(leftPtr<rightPtr)
     {
-        for(int j=i+1;j<heights.size();j++)
+        minIndex = std::min(heights[leftPtr],heights[rightPtr]);
+        maxWater = std::max(maxWater, minIndex*(rightPtr-leftPtr));
+        if(heights[leftPtr]>heights[rightPtr])
         {
-            minIndex = std::min(heights[i],heights[j]);
-            maxWater = std::max(maxWater, minIndex*(j-i));
+            rightPtr--;
+        }
+        else
+        {
+            leftPtr++;
         }
     }
     std::cout<<maxWater;
+
+    // //Brute Force Approach -> TC: O(n^2)
+    // for(int i=0;i<heights.size();i++)
+    // {
+    //     for(int j=i+1;j<heights.size();j++)
+    //     {
+    //         minIndex = std::min(heights[i],heights[j]);
+    //         maxWater = std::max(maxWater, minIndex*(j-i));
+    //     }
+    // }
+    // std::cout<<maxWater;
     return 0;
 }
