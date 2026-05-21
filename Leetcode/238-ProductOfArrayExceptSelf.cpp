91d7277951628601c0f1ff01e5d097dbd468144b
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-05-21 17:40:35 +0530

    Optimized Approach

--- Leetcode/238-ProductOfArrayExceptSelf.cpp
+++ Leetcode/238-ProductOfArrayExceptSelf.cpp
@@ -5,19 +5,51 @@ int main()
 {
     std::vector<int> nums = {1,2,3,4}; // Output: {24,12,8,6}
     std::vector<int> productArray(nums.size(),1);
-
-    // Brute Force Approach -> TC: 0(n^2)
-    for(int i=0;i<nums.size();i++)
+    
+    // Most Optimal Solution -> O(n), SC: O(1). For better understanding, see the below less optimal solution
+    for(int i=1;i<nums.size();i++)
     {
-        for(int j=0;j<nums.size();j++)
-        {
-            if(i==j)
-            {
-                continue;
-            }
-            productArray[i] *= nums[j];
-        }
+        productArray[i] = productArray[i-1]*nums[i-1];
     }
+    int product = 1;
+    for(int i=nums.size()-2;i>=0;i--)
+    {
+        product *= nums[i+1];
+        productArray[i] *= product;
+    }
+
+    // // Optimized Approach, but SC is not optmized -> TC: O(n), SC: O(n)
+    // std::vector<int> prefixProduct(nums.size(),1);
+    // std::vector<int> suffixProduct(nums.size(),1);
+    // int product = 1;
+    // for(int i=1;i<nums.size();i++)
+    // {
+    //     product *= nums[i-1];
+    //     prefixProduct[i] = product;
+    // }
+    // product = 1;
+    // for(int i=nums.size()-2;i>=0;i--)
+    // {
+    //     product *= nums[i+1];
+    //     suffixProduct[i] = product;
+    // }
+    // for(int i=0;i<nums.size();i++)
+    // {
+    //     productArray.at(i) = prefixProduct[i] * suffixProduct[i];
+    // }
+
+    // // Brute Force Approach -> TC: 0(n^2)
+    // for(int i=0;i<nums.size();i++)
+    // {
+    //     for(int j=0;j<nums.size();j++)
+    //     {
+    //         if(i==j)
+    //         {
+    //             continue;
+    //         }
+    //         productArray[i] *= nums[j];
+    //     }
+    // }
 
     for(int i=0;i<productArray.size();i++)
     {
