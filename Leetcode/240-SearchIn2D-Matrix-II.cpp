13b21dd8b31306f527a4a58b6943a9c43ed3a44c
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-06-03 22:44:48 +0530

    Mid will be corner element

--- Leetcode/240-SearchIn2D-Matrix-II.cpp
+++ Leetcode/240-SearchIn2D-Matrix-II.cpp
@@ -3,28 +3,6 @@
 #include<iostream>
 #include<vector>
 
-int searchInRow(std::vector<int> row, int rowSize, int target)
-{
-    int start=0,end=rowSize-1;
-    while(start<=end)
-    {
-        int mid = start + (end-start)/2;
-        if(row[mid]==target)
-        {
-            return mid;
-        }
-        else if(row[mid]>target)
-        {
-            start=mid+1;
-        }
-        else
-        {
-            end=mid-1;
-        }
-    }
-    return -1;
-}
-
 std::pair<int,int> twoDSearch(std::vector<std::vector<int>> matrix,int target)
 {
     int rowSize = matrix.size(), colSize = matrix[0].size();
