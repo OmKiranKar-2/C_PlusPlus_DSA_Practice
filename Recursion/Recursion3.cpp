f0dfbf743def697951e1dba9ebade5879e3bf271
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-06-14 16:48:12 +0530

    Check Array is Sorted or Not

--- Recursion/Recursion3.cpp
+++ Recursion/Recursion3.cpp
@@ -1,7 +1,25 @@
+// To check is a array is sorted or not in ascending order
 #include<iostream>
+#include<vector>
+
+// TC: O(n), SC: O(n)
+int checkArray(std::vector<int>& vec, int size)
+{
+    if(size==1 || size==0)
+    {
+        return true;
+    }
+    if(vec[size-1]>=vec[size-2])
+    {
+        return checkArray(vec,size-1);
+    }
+    return false;
+    // or return vec[size-1]>=vec[size-2] && checkArray(vec,size-1);
+}
 
 int main()
 {
-    
+    std::vector<int> vec = {1,2,3,4,5};
+    std::cout<<"Array Sorted Status: "<<checkArray(vec,vec.size());
     return 0;
 }
