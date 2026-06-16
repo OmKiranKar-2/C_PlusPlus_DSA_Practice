78d08bd4d903741eb8c312ab3c78ad5142a7fbf5
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-06-17 01:04:28 +0530

    Recursion Backtracking

--- Leetcode/46-Permutations.cpp
+++ Leetcode/46-Permutations.cpp
@@ -4,15 +4,26 @@
 #include<vector>
 #include<algorithm>
 
-void helperFunction(std::vector<int>& nums, std::vector<std::vector<int>>& answer)
+// TC: O(n * n!), SC: O(n + n!) = O(n!)
+void helperFunction(std::vector<int>& nums, int currentIdx, std::vector<std::vector<int>>& answer)
 {
-    
+    if(currentIdx==nums.size())
+    {
+        answer.emplace_back(nums);
+        return;
+    }
+    for(int i=currentIdx;i<nums.size();i++)
+    {
+        std::swap(nums[i],nums[currentIdx]);
+        helperFunction(nums,currentIdx+1,answer);
+        std::swap(nums[i],nums[currentIdx]); // To revert nums to original position
+    }
 }
 
 std::vector<std::vector<int>> permutations(std::vector<int>& nums)
 {
     std::vector<std::vector<int>> answer;
-    helperFunction(nums, answer);
+    helperFunction(nums, 0, answer);
     return answer;
 }
 
@@ -65,7 +76,7 @@ std::vector<std::vector<int>> permutations(std::vector<int>& nums)
 
 int main()
 {
-    std::vector<int> nums={1,2,3,4}; // Output: {{1,2,3},{1,3,2},{2,1,3},{2,3,1},{3,1,2},{3,2,1}}
+    std::vector<int> nums={1,2,3}; // Output: {{1,2,3},{1,3,2},{2,1,3},{2,3,1},{3,1,2},{3,2,1}}
     std::vector<std::vector<int>> answer = permutations(nums);
     std::cout<<"{ ";
     for(std::vector<int> i: answer)
