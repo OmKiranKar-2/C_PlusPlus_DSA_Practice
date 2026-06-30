213ff83dcbaaf49a6134e76441c32a6adbcaf7ef
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-07-01 00:42:35 +0530

    Recursion Backtracking

--- Leetcode/37-SudokuSolver.cpp
+++ Leetcode/37-SudokuSolver.cpp
@@ -4,7 +4,7 @@
 #include<vector>
 
 // TC: O(9^Number of Empty Cells)
-bool isSafe(std::vector<std::vector<char>>& board, int& row, int& col, int& i)
+bool isSafe(std::vector<std::vector<char>>& board, int& row, int& col, char& i)
 {
     for(int j=0;j<9;j++)
     {
@@ -13,7 +13,7 @@ bool isSafe(std::vector<std::vector<char>>& board, int& row, int& col, int& i)
             return false;
         }
     }
-    for(int j = row-(row%3); j< row-(row%3)+3; j++) // Check 3x3 matrix
+    for(int j = row-(row%3); j< row-(row%3)+3; j++) // Check 3x3 matrix // or j=(row/3)*3; j< (row/3)*3 + 3;j++
     {
         for(int k = col-(col%3); k< col-(col%3)+3; k++)
         {
@@ -27,35 +27,35 @@ bool isSafe(std::vector<std::vector<char>>& board, int& row, int& col, int& i)
 }
 
 
-void helperFunction(std::vector<std::vector<char>>& board, int row, int col)
+bool helperFunction(std::vector<std::vector<char>>& board, int row, int col)
 {
+    if(row == 9)
+    {
+        return true;
+    }
     int nextRow = row, nextCol = col+1;
     if(nextCol==9)
     {
         nextRow = row+1;
         nextCol = 0;
     }
-    if(nextRow==9)
+    if(board[row][col]!='.')
     {
-        return;
+        return helperFunction(board,nextRow,nextCol);
     }
-    if(board[row][col]=='.')
+    for(char i='1';i<='9';i++)
     {
-        for(int i=1;i<=9;i++)
+        if(isSafe(board,row,col,i))
         {
-            if(isSafe(board,row,col,i))
+            board[row][col]=i;
+            if(helperFunction(board,nextRow,nextCol))
             {
-                board[row][col]=i;
-                helperFunction(board,nextRow,nextCol);
-                board[row][col]='.';
+                return true;
             }
+            board[row][col]='.';
         }
     }
-    else
-    {
-        helperFunction(board,nextRow,nextCol);
-    }
-
+    return false;
 }
 
 void sudokuSolver(std::vector<std::vector<char>>& board)
