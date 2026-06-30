ef909497bb99d0091edee612a4a283ff704b6334
OmKiranKar-2 <karomkiranofficial4781@gmail.com>
2026-06-30 23:43:40 +0530

    Incomplete

--- Leetcode/37-SudokuSolver.cpp
+++ Leetcode/37-SudokuSolver.cpp
@@ -4,15 +4,63 @@
 #include<vector>
 
 // TC: O(9^Number of Empty Cells)
-
-void helperFunction(std::vector<std::vector<char>>& board, int& row, int& col)
+bool isSafe(std::vector<std::vector<char>>& board, int& row, int& col, int& i)
 {
+    for(int j=0;j<9;j++)
+    {
+        if(board[row][j]==i || board[j][col]==i) // Horizontal and Vertical Check
+        {
+            return false;
+        }
+    }
+    for(int j = row-(row%3); j< row-(row%3)+3; j++) // Check 3x3 matrix
+    {
+        for(int k = col-(col%3); k< col-(col%3)+3; k++)
+        {
+            if(board[j][k]==i)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+void helperFunction(std::vector<std::vector<char>>& board, int row, int col)
+{
+    int nextRow = row, nextCol = col+1;
+    if(nextCol==9)
+    {
+        nextRow = row+1;
+        nextCol = 0;
+    }
+    if(nextRow==9)
+    {
+        return;
+    }
+    if(board[row][col]=='.')
+    {
+        for(int i=1;i<=9;i++)
+        {
+            if(isSafe(board,row,col,i))
+            {
+                board[row][col]=i;
+                helperFunction(board,nextRow,nextCol);
+                board[row][col]='.';
+            }
+        }
+    }
+    else
+    {
+        helperFunction(board,nextRow,nextCol);
+    }
 
 }
 
 void sudokuSolver(std::vector<std::vector<char>>& board)
 {
-
+    helperFunction(board,0,0);
 }
 
 int main()
